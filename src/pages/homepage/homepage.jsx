4afd873ec39e1ca8afa712ad0783586ee95611e8
Junlong-Ye <junlong.ye1@gmail.com>
2021-05-19 20:36:25 +0100

    fix autoPlay

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,32 +1,31 @@
-import video from '../../assets/micro.mp4'; 
-import './homepage.styles.scss'; 
+import video from "../../assets/micro.mp4";
+import "./homepage.styles.scss";
 
 const HomePage = () => {
-    return (
-        <div className='homepage'>
-            <div className='video'>
-                <video 
-                class='microloans' 
-                src={video}
-                autoplay 
-                loop 
-                muted
-                ></video>
-            </div>
-            <div className='content'>
-                <h1> Fund a cause,
-                    <br/> 
-                    <span class='change'>change a life</span></h1>
-                <p> With microLend, you can lend as little as $25
-                    and make a big change in someone's life
-                </p>
-                <button>Find a cause</button>
-            </div>
-            <div className='causes'> 
-                <h1>Support casues you care about</h1>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="homepage">
+      <div className="video">
+        <video className="microloans" src={video} autoPlay loop muted></video>
+      </div>
+      <div className="content">
+        <h1>
+          {" "}
+          Fund a cause,
+          <br />
+          <span className="change">change a life</span>
+        </h1>
+        <p>
+          {" "}
+          With microLend, you can lend as little as $25 and make a big change in
+          someone's life
+        </p>
+        <button>Find a cause</button>
+      </div>
+      <div className="causes">
+        <h1>Support casues you care about</h1>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage; 
+export default HomePage;
