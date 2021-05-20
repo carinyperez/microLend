280ce450135e4ebf1cb65546c95f0d87fc8ba25e
Cariny Perez <carinyperez@Carinys-MacBook-Pro.local>
2021-05-19 17:13:23 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
-const express = require('express'); 
-const PORT = process.env.PORT || 3001; 
+const express = require('express');
+const pg = require('pg'); 
+const path = require('path'); 
+
+// connect to postgres database 
+const pool = new pg.Pool({
+    host: 'localhost', 
+    port: 5432,
+    database: 'microLend',
+    user:'carinyperez',
+    password: ''
+})
+
+// check connection 
+pool.query('SELECT 1 + 1')
+    .then((res)=> {
+        console.log(res)
+    })
+
+const PORT = process.env.PORT || 5000; 
 const app = express(); 
 
-app.get('/api', (req, res) => {
-    res.send('Hello from the server')
+// app.use(express.urlencoded)({
+//     extended: true
+// })
+
+// route handlers 
+app.get('/users', async(req, res) => {
+    const {rows} = await pool.query(`
+        SELECT * FROM users; 
+    `)
+    res.send(rows); 
 })
+
+// Serve static assets in production 
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder 
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname),'client', 'build', 'index.html')
+    })
+}
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 })
