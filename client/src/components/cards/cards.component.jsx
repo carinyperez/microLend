3010245d06806d90407e1918e635003a96db8578
Junlong-Ye <junlong.ye1@gmail.com>
2021-05-22 21:56:04 +0100

    add progress bar

--- client/src/components/cards/cards.component.jsx
+++ client/src/components/cards/cards.component.jsx
@@ -1,34 +1,35 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import 
-{Card, CardHeader, CardTitle, CardBody, CardActions, CardImage, CardSubtitle,Avata } 
-from '@progress/kendo-react-layout';
-import './cards.styles.scss'; 
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  CardActions,
+  CardImage,
+  CardSubtitle,
+  Avata,
+} from "@progress/kendo-react-layout";
+import { ProgressBar } from "@progress/kendo-react-progressbars";
+import "./cards.styles.scss";
 
 const Cards = (props) => {
-        return (
-            <div>
-                <Card className='card'>
-                    <CardImage src={props.image} alt='card'/>
-                    <CardTitle>{props.title}
-                    <p>{props.amount}</p>
-                    </CardTitle>
-                    <CardBody>
-                        {props.desc}
-                    </CardBody>
-                    <CardActions>
-                        <button>{props.button}</button>
-                    </CardActions>
-                </Card>   
-            </div>
+  return (
+    <div>
+      <Card className="card">
+        <CardImage src={props.image} alt="card" />
+        <CardTitle>
+          {props.title}
+          <p>{props.amount}</p>
+        </CardTitle>
+        <CardBody>{props.desc}</CardBody>
+        <ProgressBar value={props.percentage} />
+        <CardActions>
+          <button>{props.button}</button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
-        )
-}
-    
-
-
-export default Cards; 
-
-
-
-
+export default Cards;
--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -3,30 +3,31 @@ import loan from "../../assets/loan.png";
 import borrow from "../../assets/borrow.png";
 import mentors from "../../assets/mentors.png";
 import "./homepage.styles.scss";
-import Cards from '../../components/cards/cards.component';
-import card1 from '../../assets/card1.jpeg'; 
-import card2 from '../../assets/card2.jpeg'; 
-import card3 from '../../assets/card3.jpeg'; 
-import card4 from '../../assets/card4.jpeg'; 
-
+import Cards from "../../components/cards/cards.component";
+import card1 from "../../assets/card1.jpeg";
+import card2 from "../../assets/card2.jpeg";
+import card3 from "../../assets/card3.jpeg";
+import card4 from "../../assets/card4.jpeg";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="home">
-        <img src={home} alt='home'></img>
+        <img src={home} alt="home"></img>
         <p>
           {" "}
-          With microLend, you can lend as little as $25 
-          <br/>
+          With microLend, you can lend as little as $25
+          <br />
           and make a big change in someone's life
         </p>
       </div>
-      <div className='content'>
-        <div className='loan'> 
-          <p><img src={loan} alt='loan'></img></p>
+      <div className="content">
+        <div className="loan">
+          <p>
+            <img src={loan} alt="loan"></img>
+          </p>
           <h1>Lender</h1>
-          <div className='line'></div>
+          <div className="line"></div>
           <h2>Find a borrower</h2>
           <p>Browse by category and find a cause to support</p>
           <h2>Pay with Cash or Crypto</h2>
@@ -34,41 +35,75 @@ const HomePage = () => {
           <h2>Get repaid</h2>
           <p>microLend borrowers have a 99% repayment rate</p>
         </div>
-        <div className='borrow'> 
-          <p><img src={borrow} alt='borrow'></img></p>
+        <div className="borrow">
+          <p>
+            <img src={borrow} alt="borrow"></img>
+          </p>
           <h1>Borrower</h1>
-          <div className='line'></div>
-          <br/>
+          <div className="line"></div>
+          <br />
           <h3>Apply</h3>
           <p>Fill out our online application</p>
           <h2>Fundraise</h2>
-          <p>Go public on microLend to share your campaign with millions of lenders online</p>
+          <p>
+            Go public on microLend to share your campaign with millions of
+            lenders online
+          </p>
           <h2>Repay</h2>
           <p>Repay your loan to be eligible for more loans</p>
         </div>
-        <div className='mentors'> 
-          <p><img src={mentors} alt='mentors'></img></p>
+        <div className="mentors">
+          <p>
+            <img src={mentors} alt="mentors"></img>
+          </p>
           <h1>Mentors</h1>
-          <div className='line'></div>
-          <br/>
+          <div className="line"></div>
+          <br />
           <h2>Find a mentor</h2>
           <p>Choose a mentor from the list of mentors</p>
           <h2>Send a message</h2>
           <p>Send a request to the mentor for guidance</p>
           <h2>Get trained</h2>
-          <p>Our mentors can help with things like business strategy,market research, business planning</p>
+          <p>
+            Our mentors can help with things like business strategy,market
+            research, business planning
+          </p>
         </div>
       </div>
       <h1>Support causes you care about</h1>
       <div className="causes">
-        <Cards title='An-Nisa UI Islam' image={card1} button='View loan' amount='$100'
-        desc='This loan will help buy computers for an after-school program'/>
-        <Cards title='Rengvo' image={card2} button='View loan' amount='$200'
-        desc='This loan will help buy seeds for next harvesting season'/>
-        <Cards title='Mandingu' image={card3} button='View loan' amount='$25'
-        desc='This loan will help plant fruit trees and the fruit will be sold'/>
-        <Cards title='Del Sol Group' image={card4} button='View loan' amount='$2500'
-        desc='This loan will help buy the materials to build a school'/>
+        <Cards
+          title="An-Nisa UI Islam"
+          image={card1}
+          button="View loan"
+          amount="$100"
+          desc="This loan will help buy computers for an after-school program"
+          percentage="40%"
+        />
+        <Cards
+          title="Rengvo"
+          image={card2}
+          button="View loan"
+          amount="$200"
+          desc="This loan will help buy seeds for next harvesting season"
+          percentage="60%"
+        />
+        <Cards
+          title="Mandingu"
+          image={card3}
+          button="View loan"
+          amount="$25"
+          desc="This loan will help plant fruit trees and the fruit will be sold"
+          percentage="50%"
+        />
+        <Cards
+          title="Del Sol Group"
+          image={card4}
+          button="View loan"
+          amount="$2500"
+          desc="This loan will help buy the materials to build a school"
+          percentage="70%"
+        />
       </div>
     </div>
   );
