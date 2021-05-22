7532f69f05f396b4cd0a2201e16c84c22e0db99c
Cariny Perez <carinyperez@Carinys-MacBook-Pro.local>
2021-05-21 17:59:53 -0700

    Changing homepage

--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -19,59 +19,44 @@ const HomePage = () => {
       <div className='content'>
         <div className='loan'> 
           <p><img src={loan} alt='loan'></img></p>
-          <h2>Lender</h2>
+          <h1>Lender</h1>
           <div className='line'></div>
-          <h3>Find a borrower</h3>
+          <h2>Find a borrower</h2>
           <p>Browse by category and find a cause to support</p>
-          <h3>Pay with Cash or Crypto</h3>
+          <h2>Pay with Cash or Crypto</h2>
           <p>Fund with either cash or crypto account</p>
-          <h3>Get repaid</h3>
+          <h2>Get repaid</h2>
           <p>microLend borrowers have a 99% repayment rate</p>
         </div>
         <div className='borrow'> 
           <p><img src={borrow} alt='borrow'></img></p>
-          <h2>Borrower</h2>
+          <h1>Borrower</h1>
           <div className='line'></div>
           <br/>
           <h3>Apply</h3>
           <p>Fill out our online application</p>
-          <h3>Fundraise</h3>
+          <h2>Fundraise</h2>
           <p>Go public on microLend to share your campaign with millions of lenders online</p>
-          <h3>Repay</h3>
+          <h2>Repay</h2>
           <p>Repay your loan to be eligible for more loans</p>
         </div>
         <div className='mentors'> 
           <p><img src={mentors} alt='mentors'></img></p>
-          <h2>Mentors</h2>
+          <h1>Mentors</h1>
           <div className='line'></div>
           <br/>
-          <h3>Find a mentor</h3>
+          <h2>Find a mentor</h2>
           <p>Choose a mentor from the list of mentors</p>
-          <h3>Send a message</h3>
+          <h2>Send a message</h2>
           <p>Send a request to the mentor for guidance</p>
-          <h3>Get trained</h3>
+          <h2>Get trained</h2>
           <p>Our mentors can help with things like business strategy,market research, business planning</p>
         </div>
       </div>
-      {/* <div className="content">
-        <h1>
-          {" "}
-          Fund a cause,
-          <br />
-          <span className="change">change a life</span>
-        </h1>
-        <p>
-          {" "}
-          With microLend, you can lend as little as $25 and make a big change in
-          someone's life
-        </p>
-        <button>Find a cause</button>
-      </div> */}
       <div className="causes">
         <h1>Support casues you care about</h1>
       </div>
     </div>
   );
 };
-
 export default HomePage;
